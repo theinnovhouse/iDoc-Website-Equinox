c738530852792ded85becbe8c9e82c79602eddea
vidushig08 <vidushi.gupta2019@vitstudent.ac.in>
2021-05-01 17:50:03 +0530

    firebase

---
+++ js/firebase.js
@@ -0,0 +1,22 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+apiKey: "AIzaSyBAg9nx_WmLS0G_b-9kk8sVJN5DDBh3GLM",
+authDomain: "idoc-15037.firebaseapp.com",
+databaseURL: "https://idoc-15037-default-rtdb.firebaseio.com",
+projectId: "idoc-15037",
+storageBucket: "idoc-15037.appspot.com",
+messagingSenderId: "131276889872",
+appId: "1:131276889872:web:0f9aa9bd35ea8bba22cf95",
+measurementId: "G-9KETN943W5"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+// The core Firebase JS SDK is always required and must be listed first
+<script src="https://www.gstatic.com/firebasejs/8.4.3/firebase-app.js"></script>
+
+// TODO: Add SDKs for Firebase products that you want to use
+//      https://firebase.google.com/docs/web/setup#available-libraries
+<script src="https://www.gstatic.com/firebasejs/8.4.3/firebase-analytics.js"></script>

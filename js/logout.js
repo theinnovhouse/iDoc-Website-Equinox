652c751aad9fe7b41afc4c332506d52b38b643b7
vidushig08 <vidushi.gupta2019@vitstudent.ac.in>
2021-05-02 01:10:12 +0530

    medistore button fixed

--- js/logout.js
+++ js/logout.js
@@ -3,10 +3,21 @@ function logout(){
     firebase.auth().signOut().then(() => {
         // Sign-out successful.
         alert("Signed Out");
-        window.location.assign("index.html");
+        window.location.assign("login.html");
 
       }).catch((error) => {
         // An error happened.
         alert(error);
       });
   }
+
+  firebase.auth().onAuthStateChanged(function(user){
+  if(user){
+      //user is signed in
+      document.getElementById("login").style.display="none";
+      document.getElementById("logout").style.display="block";
+  }else{
+    document.getElementById("login").style.display="block";
+    document.getElementById("logout").style.display="none";
+  }
+});

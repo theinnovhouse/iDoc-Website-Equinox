6a46c8cd4d6793f06b0fb4e64265445a0e9cc0b8
anmolbansal7 <abanmolbansal5@gmail.com>
2021-05-01 21:50:24 +0530

    doctor card firebase

--- js/firebase.js
+++ js/firebase.js
@@ -1,22 +1,52 @@
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-var firebaseConfig = {
-apiKey: "AIzaSyBAg9nx_WmLS0G_b-9kk8sVJN5DDBh3GLM",
-authDomain: "idoc-15037.firebaseapp.com",
-databaseURL: "https://idoc-15037-default-rtdb.firebaseio.com",
-projectId: "idoc-15037",
-storageBucket: "idoc-15037.appspot.com",
-messagingSenderId: "131276889872",
-appId: "1:131276889872:web:0f9aa9bd35ea8bba22cf95",
-measurementId: "G-9KETN943W5"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
 
-// The core Firebase JS SDK is always required and must be listed first
-<script src="https://www.gstatic.com/firebasejs/8.4.3/firebase-app.js"></script>
+//Get radio button value
+function getCheckedValue(el) {
+    for (var i = 0, length = el.length; i < length; i++) {
+      if (el[i].checked) {
+        return el[i].value;
+        break;
+      }
+    }
+    return '';
+  }
 
-// TODO: Add SDKs for Firebase products that you want to use
-//      https://firebase.google.com/docs/web/setup#available-libraries
-<script src="https://www.gstatic.com/firebasejs/8.4.3/firebase-analytics.js"></script>
+  
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyBAg9nx_WmLS0G_b-9kk8sVJN5DDBh3GLM",
+    authDomain: "idoc-15037.firebaseapp.com",
+    databaseURL: "https://idoc-15037-default-rtdb.firebaseio.com",
+    projectId: "idoc-15037",
+    storageBucket: "idoc-15037.appspot.com",
+    messagingSenderId: "131276889872",
+    appId: "1:131276889872:web:0f9aa9bd35ea8bba22cf95",
+    measurementId: "G-9KETN943W5"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+
+
+  
+  
+//To read values of the form
+function readData() {
+    let chosenSlot = getCheckedValue(document.getElementsByName('t[]'));
+    console.log(chosenSlot);
+    let doctor = "Dr. Jane";
+    let user = "Guest User"
+    writeUserData(chosenSlot, doctor, user);
+  }
+  
+  //To push values to firebase
+  function writeUserData(chosenSlot, doctor, user)
+  {
+      firebase.database().ref('appointment/').push({
+      date: chosenSlot,
+      doctor: doctor, 
+      uid: user
+    });
+    console.log("Hey");
+}
